b45da0d48941afb3f836a301312cf2bf5715fa5f
AkinaySau <akinaysau@gmail.com>
2017-06-01 10:09:53 +0300

    Action - добавлены параметры $priority и $accepted_args

--- src/Action.php
+++ src/Action.php
@@ -16,55 +16,67 @@ class Action {
 	/**
 	 * Для хука after_setup_theme
 	 *
-	 * @param callable $callback Функция срабатывающая в момент события
+	 * @param callable $callback      Функция срабатывающая в момент события
+	 * @param int      $priority      Приоритет выполнения функции
+	 * @param int      $accepted_args Число аргументов которые принимает функция
 	 */
-	public static function afterSetupTheme( $callback ) {
-		self::action( 'after_setup_theme', $callback );
+	public static function afterSetupTheme( $callback, $priority = 10, $accepted_args = 1 ) {
+		self::action( 'after_setup_theme', $callback, $priority, $accepted_args );
 	}
 
 	/**
 	 * Выполнение хука
 	 *
-	 * @param callable $hook   Хук
-	 * @param callable $action Функция обратного ызова
+	 * @param callable $hook          Хук
+	 * @param callable $action        Функция обратного ызова
+	 * @param int      $priority      Приоритет выполнения функции
+	 * @param int      $accepted_args Число аргументов которые принимает функция
 	 */
-	protected static function action( $hook, $action ) {
-		add_action( $hook, $action );
+	protected static function action( $hook, $action, $priority, $accepted_args ) {
+		add_action( $hook, $action, (int) $priority, (int) $accepted_args );
 	}
 
 	/**
 	 * Для хука init
 	 *
-	 * @param callable $callback Функция срабатывающая в момент события
+	 * @param callable $callback      Функция срабатывающая в момент события
+	 * @param int      $priority      Приоритет выполнения функции
+	 * @param int      $accepted_args Число аргументов которые принимает функция
 	 */
-	public static function init( $callback ) {
-		self::action( 'init', $callback );
+	public static function init( $callback, $priority = 10, $accepted_args = 1 ) {
+		self::action( 'init', $callback, $priority, $accepted_args );
 	}
 
 	/**
 	 * Для хука upload_mines
 	 *
-	 * @param callable $callback Функция срабатывающая в момент события
+	 * @param callable $callback      Функция срабатывающая в момент события
+	 * @param int      $priority      Приоритет выполнения функции
+	 * @param int      $accepted_args Число аргументов которые принимает функция
 	 */
-	public static function uploadMines( $callback ) {
-		self::action( 'upload_mines', $callback );
+	public static function uploadMines( $callback, $priority = 10, $accepted_args = 1 ) {
+		self::action( 'upload_mines', $callback, $priority, $accepted_args );
 	}
 
 	/**
 	 * Для хука pre_get_posts
 	 *
-	 * @param callable $callback Функция срабатывающая в момент события
+	 * @param callable $callback      Функция срабатывающая в момент события
+	 * @param int      $priority      Приоритет выполнения функции
+	 * @param int      $accepted_args Число аргументов которые принимает функция
 	 */
-	public static function preGetPosts( $callback ) {
-		self::action( 'pre_get_posts', $callback );
+	public static function preGetPosts( $callback, $priority = 10, $accepted_args = 1 ) {
+		self::action( 'pre_get_posts', $callback, $priority, $accepted_args );
 	}
 
 	/**
 	 * Для хука wp_enqueue_scripts
 	 *
-	 * @param callable $callback Функция срабатывающая в момент события
+	 * @param callable $callback      Функция срабатывающая в момент события
+	 * @param int      $priority      Приоритет выполнения функции
+	 * @param int      $accepted_args Число аргументов которые принимает функция
 	 */
-	public static function wpEnqueueScripts( $callback ) {
-		self::action( 'wp_enqueue_scripts', $callback );
+	public static function wpEnqueueScripts( $callback, $priority = 10, $accepted_args = 1 ) {
+		self::action( 'wp_enqueue_scripts', $callback, $priority, $accepted_args );
 	}
 }
