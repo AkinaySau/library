2235bca1ad7721ea5356b27aed2a4fd614220914
AkinaySau <akinaysau@gmail.com>
2017-06-07 16:05:48 +0300

    фикс Custom::loginLogo

--- src/Custom.php
+++ src/Custom.php
@@ -13,13 +13,14 @@ class Custom {
 
 	/**
 	 * @param string $path_to_img Путь к изображению логотипа
-	 * @param bool   $link Ссылка для логотипа
+	 * @param int    $width       Ширина логотипа
+	 * @param int    $height      Высота логотипа
+	 * @param bool   $link        Ссылка для логотипа
 	 * @param bool   $title
 	 */
-	public static final function loginLogo( $path_to_img, $link = false, $title = false ) {
-		$callback = function () use ( &$path_to_img, &$link, &$title ) {
-			echo '<style type="text/css">#login h1 a { background: url(' . get_stylesheet_directory_uri(
-				) . '/images/logo.png) no-repeat 0 0 !important; width:187px; height:70px; }</style>';
+	public static final function loginLogo( $path_to_img, $width = 84, $height = 84, $link = false, $title = false ) {
+		$callback = function () use ( &$path_to_img, &$width, &$height ) {
+			echo '<style type="text/css">#login h1 a { background: url(' . $path_to_img . ') no-repeat 0 0 !important; width:' . $width . 'px; height:' . $height . 'px; }</style>';
 
 		};
 		Action::loginHead( $callback );
