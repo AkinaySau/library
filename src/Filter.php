f6cb55bae5d2ee6906465f23054790b6389c6c8c
AkinaySau <akinaysau@gmail.com>
2017-07-31 10:30:29 +0300

    Добавлены фильтры и модернизированаработа добавления картинок, теперь для можно в меиа менеджере создать отдельный пункт

--- src/Filter.php
+++ src/Filter.php
@@ -2,53 +2,41 @@
 /**
  * Created by PhpStorm.
  * User: AkinaySau
- * Date: 07.06.2017
- * Time: 14:29
+ * Date: 25.05.2017
+ * Time: 9:48
  */
 
 namespace Sau\Lib;
 
-
+/**
+ * Класс описывает фильтры
+ * Каждый доступный метод класса принимает в себя функцию обратного вызова
+ */
 class Filter {
-
 	/**
-	 * Заменяет ссылку на логотипе на странице входа (wp-login.php)
+	 * Для фильтра image_size_names_choose
 	 *
-	 * @param callable $callback      Функции, которая будет обрабатывать данные.
+	 * @param callable $callback      Функция срабатывающая в момент события
 	 * @param int      $priority      Приоритет выполнения функции
 	 * @param int      $accepted_args Число аргументов которые принимает функция
 	 *
 	 * @return bool
 	 */
-	public static final function loginHeaderUrl( $callback, $priority = 10, $accepted_args = 1 ) {
-		return self::filter( 'login_headerurl', $callback, $priority, $accepted_args );
+	public static function imageSizeNamesChoose( $callback, $priority = 10, $accepted_args = 1 ) {
+		return self::action( 'image_size_names_choose', $callback, $priority, $accepted_args );
 	}
 
 	/**
-	 * Прикрепляет указанную PHP функцию к указанному хуку-фильтру.
+	 * Применение фильтра
 	 *
-	 * @param callable $tag             Название фильтра, для которого будет срабатывать функция определенная в параметре $function_to_add.
-	 * @param callable $function_to_add Функции, которая будет срабатывать для указанного в предыдущем параметре фильтра.
-	 * @param int      $priority        Приоритет выполнения функции
-	 * @param int      $accepted_args   Число аргументов которые принимает функция
-	 *
-	 * @return boolean true или false, в зависимости от того удалось или нет добавить новый фильтр.
-	 */
-	protected static final function filter( $tag, $function_to_add, $priority = 10, $accepted_args = 1 ) {
-		return add_filter( $tag, $function_to_add, $priority, $accepted_args );
-	}
-
-	/**
-	 * Заменяет аттрибут title на ссылке прикреплёной к логотипу на странице входа (wp-login.php)
-	 *
-	 * @param callable $callback      Функции, которая будет обрабатывать данные.
+	 * @param callable $filter        Фильтр
+	 * @param callable $action        Функция обратного ызова
 	 * @param int      $priority      Приоритет выполнения функции
 	 * @param int      $accepted_args Число аргументов которые принимает функция
 	 *
-	 * @return bool
+	 * @return boolean
 	 */
-	public static final function loginHeaderTitle( $callback, $priority = 10, $accepted_args = 1 ) {
-		return self::filter( 'login_headertitle', $callback, $priority, $accepted_args );
-
+	protected static function action( $filter, $action, $priority, $accepted_args ) {
+		return add_filter( $filter, $action, (int) $priority, (int) $accepted_args );
 	}
 }
