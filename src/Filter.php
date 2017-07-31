a39736e926037006dec09a67cc773c02d86d7b8a
AkinaySau <akinaysau@gmail.com>
2017-07-31 10:50:47 +0300

    fix

--- src/Filter.php
+++ src/Filter.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * User: AkinaySau
- * Date: 25.05.2017
- * Time: 9:48
+ * Date: 07.06.2017
+ * Time: 14:29
  */
 
 namespace Sau\Lib;
@@ -13,6 +13,48 @@ namespace Sau\Lib;
  * Каждый доступный метод класса принимает в себя функцию обратного вызова
  */
 class Filter {
+
+	/**
+	 * Заменяет ссылку на логотипе на странице входа (wp-login.php)
+	 *
+	 * @param callable $callback      Функции, которая будет обрабатывать данные.
+	 * @param int      $priority      Приоритет выполнения функции
+	 * @param int      $accepted_args Число аргументов которые принимает функция
+	 *
+	 * @return bool
+	 */
+	public static final function loginHeaderUrl( $callback, $priority = 10, $accepted_args = 1 ) {
+		return self::filter( 'login_headerurl', $callback, $priority, $accepted_args );
+	}
+
+	/**
+	 * Прикрепляет указанную PHP функцию к указанному хуку-фильтру.
+	 *
+	 * @param callable $tag             Название фильтра, для которого будет срабатывать функция определенная в параметре $function_to_add.
+	 * @param callable $function_to_add Функции, которая будет срабатывать для указанного в предыдущем параметре фильтра.
+	 * @param int      $priority        Приоритет выполнения функции
+	 * @param int      $accepted_args   Число аргументов которые принимает функция
+	 *
+	 * @return boolean true или false, в зависимости от того удалось или нет добавить новый фильтр.
+	 */
+	protected static final function filter( $tag, $function_to_add, $priority = 10, $accepted_args = 1 ) {
+		return add_filter( $tag, $function_to_add, $priority, $accepted_args );
+	}
+
+	/**
+	 * Заменяет аттрибут title на ссылке прикреплёной к логотипу на странице входа (wp-login.php)
+	 *
+	 * @param callable $callback      Функции, которая будет обрабатывать данные.
+	 * @param int      $priority      Приоритет выполнения функции
+	 * @param int      $accepted_args Число аргументов которые принимает функция
+	 *
+	 * @return bool
+	 */
+	public static final function loginHeaderTitle( $callback, $priority = 10, $accepted_args = 1 ) {
+		return self::filter( 'login_headertitle', $callback, $priority, $accepted_args );
+
+	}
+
 	/**
 	 * Для фильтра image_size_names_choose
 	 *
@@ -23,20 +65,6 @@ class Filter {
 	 * @return bool
 	 */
 	public static function imageSizeNamesChoose( $callback, $priority = 10, $accepted_args = 1 ) {
-		return self::action( 'image_size_names_choose', $callback, $priority, $accepted_args );
-	}
-
-	/**
-	 * Применение фильтра
-	 *
-	 * @param callable $filter        Фильтр
-	 * @param callable $action        Функция обратного ызова
-	 * @param int      $priority      Приоритет выполнения функции
-	 * @param int      $accepted_args Число аргументов которые принимает функция
-	 *
-	 * @return boolean
-	 */
-	protected static function action( $filter, $action, $priority, $accepted_args ) {
-		return add_filter( $filter, $action, (int) $priority, (int) $accepted_args );
+		return self::filter( 'image_size_names_choose', $callback, $priority, $accepted_args );
 	}
 }
