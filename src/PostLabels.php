59cfd75142472ffece1edc813d3cd28ac485a618
AkinaySau <i-wolhw@ya.ru>
2017-09-03 16:58:36 +0300

    Добавлены эксперементальные классы для типа записи и её заголовков

--- src/PostLabels.php
+++ src/PostLabels.php
@@ -1,399 +1,138 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: AkinaySau
- * Date: 27.06.2017
- * Time: 9:43
+ * Created for library.
+ * User: Sau akinaysau@gmail.ru
+ * Date: 03.09.2017
+ * Time: 13:57
  */
 
 namespace Sau\Lib;
 
-/**
- * Class create labels for Posts type
- * After sets labels call to getLabels for return labels array
- * If you use WP_Post_Type convert array to object
- */
+
 class PostLabels {
-	protected $name;
-	protected $singular_name;
-	protected $add_new;
-	protected $add_new_item;
-	protected $edit_item;
-	protected $new_item;
-	protected $view_item;
-	protected $search_items;
-	protected $not_found;
-	protected $not_found_in_trash;
-	protected $parent_item_colon;
-	protected $all_items;
-	protected $archives;
-	protected $insert_into_item;
-	protected $uploaded_to_this_item;
-	protected $featured_image;
-	protected $set_featured_image;
-	protected $remove_featured_image;
-	protected $use_featured_image;
-	protected $filter_items_list;
-	protected $items_list_navigation;
-	protected $items_list;
-	protected $menu_name;
-	protected $name_admin_bar;
-	protected $view_items;
-	protected $attributes;
-
 	/**
-	 * Init method
-	 *
-	 * @return PostLabels
+	 * основное название для типа записи, обычно во множественном числе.
 	 */
-	public static function init() {
-		return new self();
-	}
+	public $name = null;
+	/**
+	 * название для одной записи этого типа.
+	 */
+	public $singular_name = null;
+	/**
+	 * текст для добавления новой записи, как "добавить новый" у постов в
+	 * админ-панели. Если нужно использовать перевод названия, вписывайте
+	 * подобным образом: _x('Add New', 'product');
+	 */
+	public $add_new = null;
+	/**
+	 * текст заголовка у вновь создаваемой записи в админ-панели. Как "Добавить
+	 * новый пост" у постов.
+	 */
+	public $add_new_item = null;
+	/**
+	 * текст для редактирования типа записи. По умолчанию: редактировать
+	 * пост/редактировать страницу.
+	 */
+	public $edit_item = null;
+	/**
+	 * текст новой записи. По умолчанию: "Новый пост"
+	 */
+	public $new_item = null;
+	/**
+	 * текст для просмотра записи этого типа. По умолчанию: "Посмотреть
+	 * пост"/"Посмотреть страницу".
+	 */
+	public $view_item = null;
+	/**
+	 * текст для поиска по этим типам записи. По умолчанию "Найти пост"/"найти
+	 * страницу".
+	 */
+	public $search_items = null;
+	/**
+	 * текст, если в результате поиска ничего не было найдено. По умолчанию:
+	 * "Постов не было найдено"/"Страниц не было найдено".
+	 */
+	public $not_found = null;
+	/**
+	 * текст, если не было найдено в корзине. По умолчанию "Постов не было
+	 * найдено в корзине"/"Страниц не было найдено в корзине".
+	 */
+	public $not_found_in_trash = null;
+	/**
+	 * текст для родительских типов. Этот параметр не используется для не
+	 * древовидных типов записей. По умолчанию "Родительская страница".
+	 */
+	public $parent_item_colon = null;
+	/**
+	 * Все записи. По умолчанию равен menu_name
+	 */
+	public $all_items = null;
+	/**
+	 * Архивы записей. По умолчанию равен all_items
+	 */
+	public $archives = null;
+	/**
+	 * Вставить в запись
+	 */
+	public $insert_into_item = null;
+	/**
+	 * Загружено для этой записи
+	 */
+	public $uploaded_to_this_item = null;
+	/**
+	 * Миниатюра записи
+	 */
+	public $featured_image = null;
+	/**
+	 * Установить миниатюру записи
+	 */
+	public $set_featured_image = null;
+	/**
+	 * Удалить миниатюру записи
+	 */
+	public $remove_featured_image = null;
+	/**
+	 * Использовать как миниатюру записи
+	 */
+	public $use_featured_image = null;
+	/**
+	 * Фильтровать список записей
+	 */
+	public $filter_items_list = null;
+	/**
+	 * Навигация по записям
+	 */
+	public $items_list_navigation = null;
+	/**
+	 * Список записей
+	 */
+	public $items_list = null;
+	/**
+	 * Название меню. По умолчанию равен name.
+	 */
+	public $menu_name = null;
+	/**
+	 * Название в админ баре (тулбаре). По умолчанию равен singular_name.
+	 */
+	public $name_admin_bar = null;
+	/**
+	 * Название в тулбаре, для страницы архива типа записей. По умолчанию:
+	 * «View Posts» / «View Pages». С WP 4.7.
+	 */
+	public $view_items = null;
+	/**
+	 * Название для метабокса атрибутов записи (у страниц это метабокс
+	 * «Свойства страницы» - «Page Attributes»). По умолчанию: «Post
+	 * Attributes» или «Page Attributes». С WP 4.7.
+	 */
+	public $attributes = null;
 
 	/**
-	 * Generate Labels Array
+	 * Возвращает массив для использования в заголовках нового типа записей
 	 *
-	 * @return array Labels for Post
+	 * @return array
 	 */
 	public function getLabels() {
-		return array_diff( get_object_vars( $this ), [ null ] );
-	}
-
-	/**
-	 * General name for the post type, usually plural. The same and overridden by $post_type_object->label. Default is Posts/Page
-	 *
-	 * @param string $name
-	 *
-	 * @return $this
-	 */
-	public function setName( string $name ) {
-		$this->name = $name;
-
-		return $this;
-	}
-
-	/**
-	 * Name for one object of this post type. Default is Post/Page
-	 *
-	 * @param string $singular_name
-	 *
-	 * @return $this
-	 */
-	public function setSingularName( string $singular_name ) {
-		$this->singular_name = $singular_name;
-
-		return $this;
-	}
-
-	/**
-	 * The add new text. The default is "Add New" for both hierarchical and non-hierarchical post types. When internationalizing this string, please use a gettext context matching your post type. Example: _x('Add New', 'product');
-	 *
-	 * @param string $add_new
-	 *
-	 * @return $this
-	 */
-	public function setAddNew( string $add_new ) {
-		$this->add_new = $add_new;
-
-		return $this;
-	}
-
-	/**
-	 * Default is Add New Post/Add New Page.
-	 *
-	 * @param string $add_new_item
-	 *
-	 * @return $this
-	 */
-	public function setAddNewItem( string $add_new_item ) {
-		$this->add_new_item = $add_new_item;
-
-		return $this;
-	}
-
-	/**
-	 * Default is Edit Post/Edit Page.
-	 *
-	 * @param string $edit_item
-	 *
-	 * @return $this
-	 */
-	public function setEditItem( string $edit_item ) {
-		$this->edit_item = $edit_item;
-
-		return $this;
-	}
-
-	/**
-	 * Default is New Post/New Page.
-	 *
-	 * @param string $new_item
-	 *
-	 * @return $this
-	 */
-	public function setNewItem( string $new_item ) {
-		$this->new_item = $new_item;
-
-		return $this;
-	}
-
-	/**
-	 * Default is View Post/View Page.
-	 *
-	 * @param string $view_item
-	 *
-	 * @return $this
-	 */
-	public function setViewItem( string $view_item ) {
-		$this->view_item = $view_item;
-
-		return $this;
-	}
-
-	/**
-	 * Default is Search Posts/Search Pages.
-	 *
-	 * @param string $search_items
-	 *
-	 * @return $this
-	 */
-	public function setSearchItems( string $search_items ) {
-		$this->search_items = $search_items;
-
-		return $this;
-	}
-
-	/**
-	 * Default is No posts found/No pages found.
-	 *
-	 * @param string $not_found
-	 *
-	 * @return $this
-	 */
-	public function setNotFound( string $not_found ) {
-		$this->not_found = $not_found;
-
-		return $this;
-	}
-
-	/**
-	 * Default is No posts found in Trash/No pages found in Trash.
-	 *
-	 * @param string $not_found_in_trash
-	 *
-	 * @return $this
-	 */
-	public function setNotFoundInTrash( string $not_found_in_trash ) {
-		$this->not_found_in_trash = $not_found_in_trash;
-
-		return $this;
-	}
-
-	/**
-	 * This string isn't used on non-hierarchical types. In hierarchical ones the default is 'Parent Page:'.
-	 *
-	 * @param string $parent_item_colon
-	 *
-	 * @return $this
-	 */
-	public function setParentItemColon( string $parent_item_colon ) {
-		$this->parent_item_colon = $parent_item_colon;
-
-		return $this;
-	}
-
-	/**
-	 * String for the submenu. Default is All Posts/All Pages.
-	 *
-	 * @param string $all_items
-	 *
-	 * @return $this
-	 */
-	public function setAllItems( string $all_items ) {
-		$this->all_items = $all_items;
-
-		return $this;
-	}
-
-	/**
-	 * String for use with archives in nav menus. Default is Post Archives/Page Archives.
-	 *
-	 * @param string $archives
-	 *
-	 * @return $this
-	 */
-	public function setArchives( string $archives ) {
-		$this->archives = $archives;
-
-		return $this;
-	}
-
-	/**
-	 * String for the media frame button. Default is Insert into post/Insert into page.
-	 *
-	 * @param string $insert_into_item
-	 *
-	 * @return $this
-	 */
-	public function setInsertIntoItem( string $insert_into_item ) {
-		$this->insert_into_item = $insert_into_item;
-
-		return $this;
-	}
-
-	/**
-	 * String for the media frame filter. Default is Uploaded to this post/Uploaded to this page.
-	 *
-	 * @param string $uploaded_to_this_item
-	 *
-	 * @return $this
-	 */
-	public function setUploadedToThisItem( string $uploaded_to_this_item ) {
-		$this->uploaded_to_this_item = $uploaded_to_this_item;
-
-		return $this;
-	}
-
-	/**
-	 * Default is Featured Image.
-	 *
-	 * @param string $featured_image
-	 *
-	 * @return $this
-	 */
-	public function setFeaturedImage( string $featured_image ) {
-		$this->featured_image = $featured_image;
-
-		return $this;
-	}
-
-	/**
-	 * Default is Set featured image.
-	 *
-	 * @param string $set_featured_image
-	 *
-	 * @return $this
-	 */
-	public function setSetFeaturedImage( string $set_featured_image ) {
-		$this->set_featured_image = $set_featured_image;
-
-		return $this;
-	}
-
-	/**
-	 * Default is Remove featured image
-	 *
-	 * @param string $remove_featured_image
-	 *
-	 * @return $this
-	 */
-	public function setRemoveFeaturedImage( string $remove_featured_image ) {
-		$this->remove_featured_image = $remove_featured_image;
-
-		return $this;
-	}
-
-	/**
-	 * Default is Use as featured image
-	 *
-	 * @param string $use_featured_image
-	 *
-	 * @return $this
-	 */
-	public function setUseFeaturedImage( string $use_featured_image ) {
-		$this->use_featured_image = $use_featured_image;
-
-		return $this;
-	}
-
-	/**
-	 * String for the table views hidden heading
-	 *
-	 * @param string $filter_items_list
-	 *
-	 * @return $this
-	 */
-	public function setFilterItemsList( string $filter_items_list ) {
-		$this->filter_items_list = $filter_items_list;
-
-		return $this;
-	}
-
-	/**
-	 * String for the table pagination hidden heading
-	 *
-	 * @param string $items_list_navigation
-	 *
-	 * @return $this
-	 */
-	public function setItemsListNavigation( string $items_list_navigation ) {
-		$this->items_list_navigation = $items_list_navigation;
-
-		return $this;
-	}
-
-	/**
-	 * String for the table hidden heading
-	 *
-	 * @param string $items_list
-	 *
-	 * @return $this
-	 */
-	public function setItemsList( string $items_list ) {
-		$this->items_list = $items_list;
-
-		return $this;
-	}
-
-	/**
-	 * Default is the same as `name`
-	 *
-	 * @param string $menu_name
-	 *
-	 * @return $this
-	 */
-	public function setMenuName( string $menu_name ) {
-		$this->menu_name = $menu_name;
-
-		return $this;
-	}
-
-	/**
-	 * String for use in New in Admin menu bar. Default is the same as `singular_name`
-	 *
-	 * @param string $name_admin_bar
-	 *
-	 * @return $this
-	 */
-	public function setNameAdminBar( string $name_admin_bar ) {
-		$this->name_admin_bar = $name_admin_bar;
-
-		return $this;
-	}
-
-	/**
-	 * Label for viewing post type archives. Default is 'View Posts' / 'View Pages'.
-	 *
-	 * @param string $view_items
-	 *
-	 * @return $this
-	 */
-	public function setViewItems( string $view_items ) {
-		$this->view_items = $view_items;
-
-		return $this;
-	}
-
-	/**
-	 * Label for the attributes meta box. Default is 'Post Attributes' / 'Page Attributes'.
-	 *
-	 * @param string $attributes
-	 *
-	 * @return $this
-	 */
-	public function setAttributes( string $attributes ) {
-		$this->attributes = $attributes;
-
-		return $this;
+		return get_object_vars( $this );
 	}
 }
--- src/PostType.php
+++ src/PostType.php
@@ -1,73 +1,247 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: AkinaySau
- * Date: 29.06.2017
- * Time: 12:03
+ * Created for library.
+ * User: Sau akinaysau@gmail.ru
+ * Date: 03.09.2017
+ * Time: 14:22
  */
 
 namespace Sau\Lib;
 
 
-use WP_Error;
-use WP_Post_Type;
-
-final class PostType {
-
-	public $post_object;
-	protected $post_type;
-
+/**
+ * Класс для описания нового типа записей.
+ * После создания класаа необходимо определить входные данные и вызвать метод
+ * создания типа записи
+ *
+ * @see https://wp-kama.ru/function/register_post_type
+ */
+class PostType {
 	/**
-	 * Constructor.
+	 * Имя типа записи помеченное для перевода на другой язык.
 	 *
-	 * Will populate object properties from the provided arguments and assign other
-	 * default properties based on that information.
-	 *
-	 * @since  4.6.0
-	 * @access public
-	 *
-	 * @see    register_post_type()
-	 *
-	 * @param string       $post_type Post type key.
-	 * @param array|string $args      Optional. Array or string of arguments for registering a post type.
-	 *                                Default empty array.
+	 * @var string
 	 */
-	public function __construct( $post_type, $args = [] ) {
-		$this->post_type   = $post_type;
-		$this->post_object = new WP_Post_Type( $this->post_type, $args );
-	}
-
+	public $label;
 	/**
-	 * @see WP_Post_Type
+	 * Объект содержащий ярлыки для типа записи
 	 *
-	 * @param string       $post_type Post type key.
-	 * @param array|string $args      Optional. Array or string of arguments for registering a post type.
-	 *                                Default empty array.
-	 *
-	 * @return PostType
+	 * @var PostLabels
 	 */
-	public static function init( $post_type, $args = [] ) {
-		return new self( $post_type, $args );
+	public $labels;
+	/**
+	 * Короткое описание типа записи.
+	 *
+	 * @var string
+	 */
+	public $description;
+	/**
+	 * Определяет является ли тип записи публичным или нет. На основе этого
+	 * параметра строятся много других, т.е. это своего рода пред-установка для
+	 * следующих параметров:
+	 *
+	 *  false
+	 *      show_ui = false - не показывать пользовательский интерфейс (UI)
+	 *      для этого типа записей
+	 *
+	 *      publicly_queryable = false - запросы относящиеся к этому типу
+	 *      записей не будут работать в шаблоне
+	 *
+	 *      exclude_from_search = true - этот тип записей не будет
+	 *      учитываться при поиске по сайту
+	 *
+	 *      show_in_nav_menus = false - этот тип записей будет спрятан из
+	 *      выбора меню навигации
+	 *
+	 *  true
+	 *      show_ui = true
+	 *      publicly_queryable = true
+	 *      exclude_from_search = false
+	 *      show_in_nav_menus = true
+	 *
+	 * @var boolean
+	 */
+	public $public;
+	/**
+	 * @var boolean
+	 */
+	public $publicly_queryable;
+	/**
+	 * @var boolean
+	 */
+	public $exclude_from_search;
+	/**
+	 * @var boolean
+	 */
+	public $show_ui;
+	/**
+	 * @var string|boolean
+	 */
+	public $show_in_menu;
+	/**
+	 * @var boolean
+	 */
+	public $show_in_admin_bar;
+	/**
+	 * @var boolean
+	 */
+	public $show_in_nav_menus;
+	/**
+	 * @var boolean
+	 */
+	public $show_in_rest;
+	/**
+	 * @var string
+	 */
+	public $rest_base;
+	/**
+	 * @var string
+	 */
+	public $rest_controller_class;
+	/**
+	 *Позиция где должно расположится меню нового типа записи:
+	 *
+	 *      1 — в самом верху меню
+	 *      2-3 — под «Консоль»
+	 *      4-9 — под «Записи»
+	 *      10-14 — под «Медиафайлы»
+	 *      15-19 — под «Ссылки»
+	 *      20-24 — под «Страницы»
+	 *      25-59 — под «Комментарии» (по умолчанию, null)
+	 *      60-64 — под «Внешний вид»
+	 *      65-69 — под «Плагины»
+	 *      70-74 — под «Пользователи»
+	 *      75-79 — под «Инструменты»
+	 *      80-99 — под «Параметры»
+	 *      100+ — под разделителем после «Параметры»
+	 *
+	 * @var integer
+	 */
+	public $menu_position = null;
+	/**
+	 * Ссылка на картинку, которая будет использоваться для этого меню.
+	 * С выходом WordPress 3.8 появился новый пакет иконок Dashicons, который
+	 * входит в состав ядра WordPress. Это комплект из более 150 векторных
+	 * изображений. Чтобы установит одну из иконок, напишите её название в этот
+	 * параметр. Например иконка постов, называется так: dashicons-admin-post,
+	 * а ссылок dashicons-admin-links
+	 *
+	 * @var string
+	 */
+	public $menu_icon = null;
+	/**
+	 * @var string|array
+	 */
+	public $capability_type;
+	/**
+	 * @var array
+	 */
+	public $capabilities;
+	/**
+	 * @var boolean
+	 */
+	public $map_meta_cap;
+	/**
+	 * Будут ли записи этого типа иметь древовидную структуру (как постоянные
+	 * страницы).
+	 *
+	 *      true - да, будут древовидными
+	 *      false - нет, будут связаны с таксономией (категориями)
+	 *
+	 * @var boolean
+	 */
+	public $hierarchical;
+	/**
+	 * Вспомогательные поля на странице создания/редактирования этого типа
+	 * записи. Метки для вызова функции add_post_type_support().
+	 *        title - блок заголовка;
+	 *        editor - блок для ввода контента;
+	 *        author - блог выбора автора;
+	 *        thumbnail блок выбора миниатюры записи. Нужно также включить
+	 *        ддержку в установке темы post-thumbnails;
+	 *        excerpt - блок ввода цитаты;
+	 *        trackbacks - блок уведомлений;
+	 *        custom-fields - блок установки произвольных полей;
+	 *        comments - блок комментариев;
+	 *        revisions - блок ревизий (не отображается пока нет ревизий);
+	 *        page-attributes - блок атрибутов постоянных страниц (шаблон и
+	 *        древовидная связь записей, древовидность должна быть включена).
+	 *        post-formats – блок форматов записи, если они включены в теме.
+	 *
+	 * @var array
+	 */
+	public $supports;
+	/**
+	 * @var string
+	 */
+	public $register_meta_box_cb;
+	/**
+	 * @var array
+	 */
+	public $taxonomies;
+	/**
+	 * Массив зарегистрированных таксономий, которые будут связанны с этим
+	 * типом записей, например: category или post_tag.
+	 *
+	 *      TODO Пока нет таксономий
+	 *      Связать таксономии с записью можно позднее через функцию
+	 *      register_taxonomy_for_object_type().
+	 *      Таксономии нужно регистрировать с помощью функции
+	 *      register_taxonomy().
+	 *
+	 * @var string
+	 */
+	public $permalink_epmask;
+	/**
+	 * @var string|boolean
+	 */
+	public $has_archive;
+	/**
+	 * @var array|boolean
+	 */
+	public $rewrite;
+	/**
+	 * Ставим false, чтобы убрать возможность запросов или устанавливаем
+	 * название запроса для этого типа записей.
+	 *
+	 * @var string|boolean
+	 */
+	public $query_var;
+	/**
+	 * @var boolean
+	 */
+	public $can_export;
+	/**
+	 * @var boolean
+	 */
+	public $delete_with_user;
+	/**
+	 * @var boolean
+	 */
+	public $_builtin;
+	/**
+	 * @var string
+	 */
+	public $_edit_link;
+	/**
+	 * Название типа записи (максимум 20 символов). Может содержать только
+	 * строчные символы, числа, _ или -: a-z0-9_-
+	 *
+	 * @var string
+	 */
+
+	private $post_type;
+
+	public function __construct( $post_type, PostLabels $labels ) {
+		$this->post_type = $post_type;
+		$this->labels    = $labels->getLabels();
 	}
 
-	public function load() {
-		if ( $this->post_object instanceof WP_Post_Type )
-		{
-			global $wp_post_types;
-
-			if ( ! is_array( $wp_post_types ) )
-			{
-				$wp_post_types = array();
-			}
-
-			if ( ! in_array( $this->post_type, $wp_post_types ) )
-			{
-				$wp_post_types[$this->post_type] = $this->post_object;
-			}
-		}
-		else
-		{
-			new WP_Error( 'add_post_type_' . $this->post_type, __( sprintf( 'Post type(%s) is not added', $this->post_type ) ) );
-		}
+	public function register() {
+		$callback = function () {
+			register_post_type( $this->post_type, get_object_vars( $this ) );
+		};
+		Action::init( $callback );
 	}
+
 }
