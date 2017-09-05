7425e40bf6aa60ed0ac443552ae56f0d5d994e6e
AkinaySau <akinaysau@gmail.com>
2017-09-05 16:33:16 +0300

    добавлена обработка ошибок при подключении файлов

--- src/Theme.php
+++ src/Theme.php
@@ -8,12 +8,17 @@
 
 namespace Sau\Lib;
 
+use function count;
+use function implode;
+
 class Theme {
 	/**
 	 * Register theme language
 	 *
-	 * @param string $domain Text domain. Unique identifier for retrieving translated strings.
-	 * @param string $path   Optional. Path to the directory containing the .mo file. Default false.
+	 * @param string $domain Text domain. Unique identifier for retrieving
+	 *                       translated strings.
+	 * @param string $path   Optional. Path to the directory containing the .mo
+	 *                       file. Default false.
 	 *
 	 * @return void
 	 */
@@ -33,13 +38,10 @@ class Theme {
 	 */
 	public static function addFavicon( $path ) {
 
-		add_action(
-			'wp_head',
-			function () use ( &$path ) {
-				echo '<link rel="icon" href="' . $path . '" />';
-				echo '<link rel="shortcut icon" href="' . $path . '" />';
-			}
-		);
+		add_action( 'wp_head', function () use ( &$path ) {
+			echo '<link rel="icon" href="' . $path . '" />';
+			echo '<link rel="shortcut icon" href="' . $path . '" />';
+		} );
 	}
 
 	/**
@@ -48,7 +50,8 @@ class Theme {
 	 * Вы можете передать второй аргумент функции в виде массива,
 	 * в котором указать для каких типов постов разрешить миниатюры
 	 *
-	 * @param string|array $format Типы постов. Если не указано то будет добавлено ко всем
+	 * @param string|array $format Типы постов. Если не указано то будет
+	 *                             добавлено ко всем
 	 *
 	 * @return void
 	 */
@@ -60,7 +63,10 @@ class Theme {
 	 * Функция расширения
 	 *
 	 * @param string       $feature Название добавляемой возможности.
-	 * @param string|array $format  Дополнительные параметры. Для каждой возможности свои. У форматов постов тут указываем форматы, у миниатюр типы постов, где они будут работать и т.д.
+	 * @param string|array $format  Дополнительные параметры. Для каждой
+	 *                              возможности свои. У форматов постов тут
+	 *                              указываем форматы, у миниатюр типы постов,
+	 *                              где они будут работать и т.д.
 	 *
 	 * @return void
 	 */
@@ -88,7 +94,8 @@ class Theme {
 	}
 
 	/**
-	 * Если активировать эту опцию для темы, то в теме не нужно устанавливать метатег title. Он будет подключен автоматически через хук wp_head.
+	 * Если активировать эту опцию для темы, то в теме не нужно устанавливать
+	 * метатег title. Он будет подключен автоматически через хук wp_head.
 	 */
 	public static function addSupportTitleTag() {
 		self::addThemeSupport( 'title-tag' );
@@ -97,19 +104,27 @@ class Theme {
 	/**
 	 * Register extend files
 	 *
-	 * @param array|object $array Array with information about the location of the files relative to the theme
+	 * @param array|object $array Array with information about the location of
+	 *                            the files relative to the theme
 	 *
 	 * @return bool|array True if not error or array of not found files
 	 */
 	public static function addLib( $array = [] ) {
+		$error = [];
 		foreach ( $array as $file ) {
 			if ( file_exists( $path_to_file = get_stylesheet_directory() . DIRECTORY_SEPARATOR . $file ) ) {
 				require $path_to_file;
 			} else {
-				$error[] = $file;
+				$error[] = '<li>' . $file . '</li>';
 			}
 		}
 
-		return $error??true;
+		if ( count( $error ) ) {
+			$error = implode( $error );
+			$error = '<p>При подключении следующих файлов возникли проблемы: </p><u>' . $error . '</u>';
+			Notice::error( $error );
+		}
+
+		return true;
 	}
 }
